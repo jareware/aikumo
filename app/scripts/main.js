123d9af9c665b099367c3c5a03adb4cb89bef2c0
Jarno Rantanen <jarno@jrw.fi>
2014-10-22 23:35:49 +0300

    Added "smooth-scroll" for, you know, smooth scrolling between anchors.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
-console.log('\'Allo \'Allo!');
+// @see https://github.com/cferdinandi/smooth-scroll#options-and-settings
+smoothScroll.init({
+    offset: 50 // TODO: Pick a value that looks good
+});
