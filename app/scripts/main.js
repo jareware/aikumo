9f52aa5fc325e0068ed71d8e369252efa75f376b
Marja Käpyaho <marja.kapyaho@futurice.com>
2014-11-29 18:44:34 +0200

    styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,12 @@
 smoothScroll.init({
     offset: 50 // TODO: Pick a value that looks good
 });
+
+window.addEventListener('scroll', function() {
+    if (document.body.scrollTop > 20) {
+        document.querySelector('body').classList.add('minified-header');
+    }
+    else {
+        document.querySelector('body').classList.remove('minified-header');
+    }
+});
