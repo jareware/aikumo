584d6d694a7d4a5e4fba468c76679a83133d54b2
Jarno Rantanen <jarno@jrw.fi>
2014-12-14 16:16:09 +0200

    Added better scroll offset handling.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,17 @@
+// @see http://stackoverflow.com/a/8876069
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+// Note: these will have to correspond to the values in $VIEWPORTS_CONFIG
+function getHeaderCompensation() {
+    if (w > 1024) return 50; // "static"
+    if (w > 550) return 70; // "fluid"
+    return 0; // "stacked"
+}
+
 // @see https://github.com/cferdinandi/smooth-scroll#options-and-settings
 smoothScroll.init({
-    offset: 50 // TODO: Pick a value that looks good
+    offset: getHeaderCompensation()
 });
 
 window.addEventListener('scroll', function() {
